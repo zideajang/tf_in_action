693accd162b537d9d91e6e51775fe86ce6612723
jangwoo <zidea2015@163.com>
2019-10-21 06:18:21 +0800

    添加生成器和判别器

--- dcgan/network.py
+++ dcgan/network.py
@@ -1,4 +1,76 @@
 #encoding=utf-8
 
-
+"""
+"""
 import tensorflow as tf
+# 定义判别器模型
+
+def discriminator_model():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(
+        64, # 64 个过滤器，输出的深度是(depth) 是 64
+        [5,5], # 过滤器在二维的大小是(5 * 5)
+        padding='same', # same 表示输出大小不变，因此需要在外围补零 2 圈
+        input_shape=(64,64,3) #输入形状 (64,64,3) 3 是表示 RGB
+    ))
+
+    model.add(tf.keras.layers.Activation("tanh")) # 添加 Tanh 激活层
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2))) #池化层
+
+    model.add(tf.keras.layers.Conv2D(128,(5,5)))
+    model.add(tf.keras.layers.Activation("tanh")) 
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2))) #池化层
+
+    model.add(tf.keras.layers.Conv2D(128,(5,5)))
+    model.add(tf.keras.layers.Activation("tanh")) 
+    model.add(tf.keras.layers.MaxPool2D(pool_size=(2,2))) #池化层
+
+    model.add(tf.keras.layers.Flatten()) #扁平操作
+    model.add(tf.keras.layers.Dense(1024)) # 1024 个神经元的全连接层
+    model.add(tf.keras.layers.Activation("tanh")) 
+    model.add(tf.keras.layers.Dense(1)) # 1 个神经元的全连接层
+
+    model.add(tf.keras.layers.Activation("sigmoid")) # 添加 Sigmoid 层
+
+    return model
+
+# 定义生成器模型
+# 从随机数生成图片
+def generator_model():
+    model = tf.keras.models.Sequential()
+    # 输出维度是 100，输出维度(神经元个数) 是 1024 的全连接层
+    model.add(tf.keras.layers.Dense(input_dim=100,units=1024)) # 接收随机数，输入维度 100
+    model.add(tf.keras.layers.Activation("tanh"))
+    model.add(tf.keras.layers.Dense(128 * 8 * 8)) # 8192 个神经元的全连接层
+    # 数据标准化操作
+    """
+    平均的激活函数平均值趋近于 0
+    """
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Activation("tanh"))
+    # 8 x 8 像素图片
+    model.add(tf.keras.layers.Reshape((8,8,128),input_shape=(128 * 8 * 8,)))
+    # 16 x 16 像素图片
+    model.add(tf.keras.layers.UpSampling2D(size=(2,2)))
+    # 
+    model.add(tf.keras.layers.Conv2D(128,(5,5),padding='same'))
+    model.add(tf.keras.layers.Activation("tanh"))
+
+    # 32 x 32 像素图片
+    model.add(tf.keras.layers.UpSampling2D(size=(2,2)))
+    model.add(tf.keras.layers.Conv2D(128,(5,5),padding='same'))
+    model.add(tf.keras.layers.Activation("tanh"))
+
+    # 64 x 64 像素图片
+    model.add(tf.keras.layers.UpSampling2D(size=(2,2)))
+    model.add(tf.keras.layers.Conv2D(3,(5,5),padding='same'))
+    model.add(tf.keras.layers.Activation("tanh"))
+
+    return model
+
+
+
+
+
+
+
